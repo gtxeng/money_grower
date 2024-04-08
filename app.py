33372dfc0a8b90101faebb0bc5e72ca4a6f8f06a
Gui Teixeira <93358588+gtxeng@users.noreply.github.com>
2024-04-08 22:09:06 +0000

    alterei um pouquinho

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
-st.title("fokyou")
+st.title("Money Grower")
+
+def build_sidebar():
+    st.write("Ativos")
+    pass
+
+
+def build_main():
+    st.write("principal")
+    pass
+
+with st.sidebar:
+    build_sidebar()
+  
