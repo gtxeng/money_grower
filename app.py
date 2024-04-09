d668afe452113906589cecc824d6685dcaaa551b
Gui Teixeira <93358588+gtxeng@users.noreply.github.com>
2024-04-09 04:44:47 +0000

    Money Grower vai ajudar a acompanhar os investimentos cripto

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
+import requests
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from datetime import datetime
+from streamlit_extras.metric_cards import style_metric_cards
+from streamlit_extras.grid import grid
 
 st.title("Money Grower")
 
 def build_sidebar():
-    st.write("Ativos")
-    pass
+    st.image("image/MoneyGrower.png")
+    url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=500&page=1&sparkline=false'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data=response.json()
+        ticker_list = [asset['symbol'] for asset in data]
+        tickers = st.multiselect(label="Selecione os ativos", options = ticker_list, placeholder="Tokens")
+        start_date = st.date_input("De:", format = "DD/MM/YYYY", value=datetime(2022,1,2))
+        end_date = st.date_input("Até:", format= "DD/MM/YYYY", value = "today")
+        #if ticker =
+    else:
+        st.write("Erro ao acessar CoinGecko")
+
+    
 
 
 def build_main():
